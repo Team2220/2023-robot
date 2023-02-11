98f20d4c521ef8c21de61c609716840874e18e30
ethankrech <87047924+ethankrech@users.noreply.github.com>
2023-02-11 15:31:05 -0600

    Update MotionMagic Config

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -92,11 +92,11 @@ public class Arm extends SubsystemBase {
 
   public Arm() {
     /* Motion Magic Configurations */
-    wristConfig.motionAcceleration = 2000;
-    wristConfig.motionCruiseVelocity = 2000;
+    wristConfig.motionAcceleration = degreesPerSecondToEncoderTicks(10, ArmConfig.WRIST_GEAR_RATIO);
+    wristConfig.motionCruiseVelocity = degreesPerSecondToEncoderTicks(10, ArmConfig.WRIST_GEAR_RATIO);
 
-    shoulderConfig.motionCruiseVelocity = 2000;
-    shoulderConfig.motionAcceleration = 2000;
+    shoulderConfig.motionCruiseVelocity = degreesPerSecondToEncoderTicks(10, ArmConfig.SHOULDER_GEAR_RATIO);
+    shoulderConfig.motionAcceleration = degreesPerSecondToEncoderTicks(10, ArmConfig.SHOULDER_GEAR_RATIO);
 
     wrist.configAllSettings(wristConfig);
     shoulder.configAllSettings(shoulderConfig);
