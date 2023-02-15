569c959b8724f2cb08688c503af6b04c4e72ebb3
notJithin <123132870+notJithin@users.noreply.github.com>
2023-02-14 21:05:06 -0600

    autonomous code

--- src/main/java/frc/robot/auto/Mobility.java
+++ src/main/java/frc/robot/auto/Mobility.java
@@ -12,16 +12,31 @@ public class Mobility extends SequentialCommandGroup {
   public Mobility(Swerve swerve) {
     addCommands(
         new InstantCommand(
-            () ->
-                swerve.setPose2d(
-                    new Pose2d(
-                        FieldConstants.Community.chargingStationCorners[0],
-                        Rotation2d.fromDegrees(90)))));
+            () -> swerve.setPose2d(
+                new Pose2d(
+                    FieldConstants.Community.chargingStationCorners[0],
+                    Rotation2d.fromDegrees(90)))));
     addCommands(
         new GoToCommand(
             swerve,
             new Pose2d(
-                FieldConstants.Community.chargingStationCorners[2], Rotation2d.fromDegrees(90))));
+                FieldConstants.Community.chargingStationCorners[2], Rotation2d.fromDegrees(180))));
+
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(
+                FieldConstants.Community.chargingStationCorners[3], Rotation2d.fromDegrees(270))));
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(
+                FieldConstants.Community.chargingStationCorners[1], Rotation2d.fromDegrees(360))));
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(
+                FieldConstants.Community.chargingStationCorners[0], Rotation2d.fromDegrees(90))));
     for (int i = 0; i < 4; i++) {
       System.out.println(FieldConstants.Community.chargingStationCorners[i]);
     }
