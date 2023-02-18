12d85795bd0258706579c67fa62107344f426874
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2023-02-17 20:37:11 -0600

    Fix angular stuff

--- src/main/java/frc/robot/auto/TestPath.java
+++ src/main/java/frc/robot/auto/TestPath.java
@@ -1,6 +1,7 @@
 package frc.robot.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.twilight.swerve.commands.GoToCommand;
 import frc.twilight.swerve.commands.SequentialMove;
 import frc.twilight.swerve.subsystems.Swerve;
 import frc.twilight.swerve.vectors.Position;
@@ -8,12 +9,9 @@ import frc.twilight.swerve.vectors.Position;
 public class TestPath extends SequentialCommandGroup {
   public TestPath(Swerve swerve) {
     addCommands(
-      new SequentialMove(
-        swerve,
-        new Position(0, 0, 0),
-        new Position(0, 3, 0),
-        new Position(3, 3, 0)
-      )
+      new GoToCommand(swerve, new Position(0, 0, 0)),
+      new GoToCommand(swerve, new Position(0, 1, 90)),
+      new GoToCommand(swerve, new Position(1, 1, 180))
     );
   }
 }
