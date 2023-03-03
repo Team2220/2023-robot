134aa2919c254edf3d532650b57863f214a6b25f
notJithin <123132870+notJithin@users.noreply.github.com>
2023-03-02 20:16:08 -0600

    ur mom

--- src/main/java/frc/robot/auto/inProgress/LowerScore3AsFarAsPossibleBlueAuto.java
+++ src/main/java/frc/robot/auto/inProgress/LowerScore3AsFarAsPossibleBlueAuto.java
@@ -1,6 +1,6 @@
-// unfinished
+// unfinished hi
 
-package frc.robot.auto;
+package frc.robot.auto.inProgress;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
