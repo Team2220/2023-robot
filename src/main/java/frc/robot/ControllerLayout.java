8ae52807dca1f6d21868058d345aebe83577f0b9
theodoreVa <123133002+theodoreVa@users.noreply.github.com>
2023-03-04 14:24:31 -0600

    made manipulator controler

--- src/main/java/frc/robot/ControllerLayout.java
+++ src/main/java/frc/robot/ControllerLayout.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.Arm.SetArmState;
+import frc.robot.subsystems.Arm.ArmStates;
 import frc.twilight.Controller.Button;
 import frc.twilight.swerve.commands.ResetGyro;
 
@@ -37,29 +39,32 @@ public class ControllerLayout {
     );
 
     public final static Map<Button, Command> MANIPULATOR = Map.ofEntries(
-        Map.entry(Button.A, null),
-        Map.entry(Button.B, null),
-        Map.entry(Button.X, null),
-        Map.entry(Button.Y, null),
+        Map.entry(Button.A, (new SetArmState(ArmStates.MID_CUBE_NODE, RobotContainer.m_arm))),
+        Map.entry(Button.B, (new SetArmState(ArmStates.MID_CONE_NODE, RobotContainer.m_arm))),
+        Map.entry(Button.X, (new SetArmState(ArmStates.HIGH_CUBE_NODE, RobotContainer.m_arm))),
+        Map.entry(Button.Y, (new SetArmState(ArmStates.HIGH_CONE_NODE, RobotContainer.m_arm))),
 
         // Bumpers
         Map.entry(Button.LB, null),
-        Map.entry(Button.RB, null),
+        Map.entry(Button.RB, (new SetArmState(ArmStates.INTAKE, RobotContainer.m_arm))),
         
         // Joystick buttons
-        Map.entry(Button.LS, null),
-        Map.entry(Button.RS, null),
+        Map.entry(Button.LS, (new InstantCommand(() -> RobotContainer.m_arm.overrideShoulderSoftLimits(false)))),
+        Map.entry(Button.RS, (new InstantCommand(() -> RobotContainer.m_arm.overrideWristSoftLimits(false)))),
 
         // Start (right) button
-        Map.entry(Button.START, null),
+        Map.entry(Button.START, (new InstantCommand(() -> {
+            RobotContainer.m_arm.setWristToReferenceAngle();
+            RobotContainer.m_arm.setShoulderToReferenceAngle();
+        }))),
 
         // Back (left) button
-        Map.entry(Button.BACK, null),
+        Map.entry(Button.BACK, (new InstantCommand(() -> DataLogManager.log("Manipulator Problem")))),
 
         // POV buttons
-        Map.entry(Button.UP, null),
-        Map.entry(Button.DOWN, null),
-        Map.entry(Button.LEFT, null),
+        Map.entry(Button.UP,(new SetArmState(ArmStates.LOADING_STATION_CONE, RobotContainer.m_arm))),
+        Map.entry(Button.DOWN, (new SetArmState(ArmStates.TRANSIT, RobotContainer.m_arm))),
+        Map.entry(Button.LEFT, (new SetArmState(ArmStates.LOADING_STATION_CUBE, RobotContainer.m_arm))),
         Map.entry(Button.RIGHT, null)
     );
 }
