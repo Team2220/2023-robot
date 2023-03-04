92777b72681f73b1c1b7fcab2d0b81ebd3363af8
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2023-03-04 15:50:36 -0600

    Update ControllerLayout.java

--- src/main/java/frc/robot/ControllerLayout.java
+++ src/main/java/frc/robot/ControllerLayout.java
@@ -2,10 +2,13 @@ package frc.robot;
 
 import java.util.Map;
 
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.twilight.Controller;
 import frc.twilight.Controller.Button;
+import frc.twilight.swerve.commands.ResetGyro;
 
 public class ControllerLayout {
     public final static Map<Button, Command> DRIVER = Map.ofEntries(
@@ -23,16 +26,16 @@ public class ControllerLayout {
         // Map.entry(Button.RS, null),
 
         // Start (right) button
-        Map.entry(Button.START, null),
+        Map.entry(Button.START, new ResetGyro(RobotContainer.m_swerve)),
 
         // Back (left) button
-        Map.entry(Button.BACK, null),
+        Map.entry(Button.BACK, new InstantCommand(() -> DataLogManager.log("Driver Problem"))),
 
         // POV buttons
-        Map.entry(Button.UP, null),
-        Map.entry(Button.DOWN, null),
-        Map.entry(Button.LEFT, null),
-        Map.entry(Button.RIGHT, null)
+        Map.entry(Button.UP, new InstantCommand(() -> RobotContainer.m_ControllerDrive.snapDrive(0))),
+        Map.entry(Button.DOWN, new InstantCommand(() -> RobotContainer.m_ControllerDrive.snapDrive(180))),
+        Map.entry(Button.LEFT, new InstantCommand(() -> RobotContainer.m_ControllerDrive.snapDrive(90))),
+        Map.entry(Button.RIGHT, new InstantCommand(() -> RobotContainer.m_ControllerDrive.snapDrive(270)))
     );
 
 
