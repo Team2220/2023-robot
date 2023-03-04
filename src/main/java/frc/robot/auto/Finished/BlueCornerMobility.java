02b72fac1acdf4828730a6dbe933521984019248
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2023-03-04 10:55:13 -0600

    Fix formatting

--- src/main/java/frc/robot/auto/Finished/BlueCornerMobility.java
+++ src/main/java/frc/robot/auto/Finished/BlueCornerMobility.java
@@ -14,50 +14,50 @@ import frc.twilight.swerve.commands.GoToCommand;
 import frc.twilight.swerve.subsystems.Swerve;
 
 public class BlueCornerMobility extends SequentialCommandGroup {
-        public BlueCornerMobility(Swerve swerve, Intake intake) {
-                addCommands(
-                                new InstantCommand(
-                                                () -> swerve.setPose2d(
-                                                                new Pose2d(
-                                                                                new Translation2d(0, 0),
-                                                                                Rotation2d.fromDegrees(180)))));
+  public BlueCornerMobility(Swerve swerve, Intake intake) {
+    addCommands(
+        new InstantCommand(
+            () -> swerve.setPose2d(
+                new Pose2d(
+                    new Translation2d(0, 0),
+                    Rotation2d.fromDegrees(180)))));
 
-                addCommands(
-                                new AutoIntake(-.5, intake).withTimeout(2));
+    addCommands(
+        new AutoIntake(-.5, intake).withTimeout(2));
 
-                addCommands(
-                                new GoToCommand(
-                                                swerve,
-                                                new Pose2d(Units.inchesToMeters(60.5), Units.inchesToMeters(-10),
-                                                                Rotation2d.fromDegrees(
-                                                                                180))));
-                addCommands(
-                                new GoToCommand(
-                                                swerve,
-                                                new Pose2d(Units.inchesToMeters(165), Units.inchesToMeters(-10),
-                                                                Rotation2d.fromDegrees(
-                                                                                180))));
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(Units.inchesToMeters(60.5), Units.inchesToMeters(-10),
+                Rotation2d.fromDegrees(180))));
 
-                addCommands(
-                                new GoToCommand(
-                                                swerve,
-                                                new Pose2d(Units.inchesToMeters(165), Units.inchesToMeters(78),
-                                                                Rotation2d.fromDegrees(
-                                                                                180))));
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(Units.inchesToMeters(165), Units.inchesToMeters(-10),
+                Rotation2d.fromDegrees(
+                    180))));
 
-                addCommands(
-                                new GoToCommand(
-                                                swerve,
-                                                new Pose2d(Units.inchesToMeters(127), Units.inchesToMeters(78),
-                                                                Rotation2d.fromDegrees(
-                                                                                180))));
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(Units.inchesToMeters(165), Units.inchesToMeters(78),
+                Rotation2d.fromDegrees(
+                    180))));
 
-                addCommands(
-                                new GoToCommand(
-                                                swerve,
-                                                new Pose2d(Units.inchesToMeters(75), Units.inchesToMeters(78),
-                                                                Rotation2d.fromDegrees(
-                                                                                0))));
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(Units.inchesToMeters(127), Units.inchesToMeters(78),
+                Rotation2d.fromDegrees(
+                    180))));
 
-        }
+    addCommands(
+        new GoToCommand(
+            swerve,
+            new Pose2d(Units.inchesToMeters(75), Units.inchesToMeters(78),
+                Rotation2d.fromDegrees(
+                    0))));
+
+  }
 }
