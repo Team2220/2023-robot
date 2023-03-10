4ddbd13b4dbd1ab371ac68cec7995d03d6cf3ae9
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2023-03-09 20:40:20 -0600

    arm thingies

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import java.util.ArrayList;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
@@ -10,9 +11,20 @@ import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConfig;
+import frc.twilight.tunables.TunableDouble;
 
 public class Intake extends SubsystemBase {
   private TalonFX intake = new TalonFX(IntakeConfig.INTAKE_TALONFX);
+
+  private TunableDouble intakeP = new TunableDouble("IntakeP", 0, true); 
+  private TunableDouble intakeI = new TunableDouble("IntakeI", 0, true);
+  private TunableDouble intakeD = new TunableDouble("IntakeD", 0, true);
+
+  private double oldP = intakeP.getValue();
+  private double oldI = intakeI.getValue();
+  private double oldD = intakeD.getValue();
+
+  private double setPos = 0;
 
   public Intake() {
     intake.configAllSettings(new TalonFXConfiguration());
@@ -20,6 +32,11 @@ public class Intake extends SubsystemBase {
     intake.configVoltageCompSaturation(10);
 
     intake.setInverted(IntakeConfig.INTAKE_INVERTED);
+    intake.setNeutralMode(NeutralMode.Brake);
+
+    intake.config_kP(0, oldP);
+    intake.config_kI(0, oldI);
+    intake.config_kD(0, oldD);
 
     StatorCurrentLimitConfiguration statorConfig = new StatorCurrentLimitConfiguration();
     statorConfig.enable = false;
@@ -33,6 +50,32 @@ public class Intake extends SubsystemBase {
   }
 
   public void setPercentOutput(double value) {
+    if (intakeP.getValue() != oldP) {
+      intake.config_kP(0, intakeP.getValue());
+      oldP = intakeP.getValue();
+    }
+  
+    if (intakeI.getValue() != oldI) {
+      intake.config_kI(0, intakeI.getValue());
+      oldI = intakeI.getValue();
+    }
+  
+    if (intakeD.getValue() != oldD) {
+      intake.config_kD(0, intakeD.getValue());
+      oldD = intakeD.getValue();
+    }
+
+    if (value == 0) {
+      if (setPos == 0) 
+        setPos = intake.getSelectedSensorPosition();
+
+      intake.set(TalonFXControlMode.Position, setPos);
+
+      return;
+    }
+
+    setPos = 0;
+
     intake.set(TalonFXControlMode.PercentOutput, value);
   }
 
---
+++ src/main/java/frc/twilight/tunables/TunableBoolean.java
@@ -0,0 +1,50 @@
+package frc.twilight.tunables;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
+
+public class TunableBoolean {
+  private boolean defaultValue;
+  private GenericEntry shuffleboard;
+  private SimpleWidget shuffleboardWidget;
+
+  /**
+   * Creates a TunableDouble. It can be enabled and disabled (Use defaultValue)
+   *
+   * @param name
+   * @param defaultValue
+   * @param tunable
+   */
+  public TunableBoolean(String name, boolean defaultValue, boolean tunable, String tab) {
+    this.defaultValue = defaultValue;
+
+    if (tunable) {
+      shuffleboardWidget = Shuffleboard.getTab(tab).add(name, defaultValue);
+
+      shuffleboard = shuffleboardWidget.getEntry();
+    } else {
+      shuffleboard = null;
+    }
+  }
+
+  public TunableBoolean(String name, boolean defaultValue, boolean tunable) {
+    this(name, defaultValue, tunable, "Tunables");
+  }
+
+  public TunableBoolean setSpot(int x, int y) {
+    if (shuffleboard != null) {
+      shuffleboardWidget.withPosition(x, y);
+    }
+
+    return this;
+  }
+
+  /**
+   * @return Value as a double
+   */
+  public boolean getValue() {
+    if (shuffleboard != null) return shuffleboard.getBoolean(defaultValue);
+    return defaultValue;
+  }
+}
