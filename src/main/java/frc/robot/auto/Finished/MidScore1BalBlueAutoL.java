317f708d47606625dadfbbfa76234f57c6855dc7
notJithin <123132870+notJithin@users.noreply.github.com>
2023-03-15 20:00:05 -0500

    .

--- src/main/java/frc/robot/auto/Finished/MidScore1BalBlueAutoL.java
+++ src/main/java/frc/robot/auto/Finished/MidScore1BalBlueAutoL.java
@@ -33,18 +33,19 @@ public class MidScore1BalBlueAutoL extends SequentialCommandGroup {
         new GoToCommand(swerve, new Pose2d(5.117751, 4.576699, Rotation2d.fromDegrees(0))),
         new SetArmState(ArmStates.INTAKE, arm),
         new GoToCommand(swerve, new Pose2d(7.070376, 4.576699, Rotation2d.fromDegrees(0)))
-            .alongWith(new AutoIntake(-.5, intake).withTimeout(5)), 
+            .alongWith(new AutoIntake(-.5, intake).withTimeout(2.5)), 
         new SetArmState(ArmStates.TRANSIT, arm),    
         new GoToCommand(swerve,
             new Pose2d(Units.inchesToMeters(54.361250 + 16.291382), Units.inchesToMeters(174.185000),
                 Rotation2d.fromDegrees(180))),
         new SetArmState(ArmStates.MID_CUBE_NODE, arm).alongWith(new AutoIntake(-.5, intake).withTimeout(.2)),
         new AutoIntake(.5, intake).withTimeout(1),
+        new SetArmState(ArmStates.TRANSIT, arm),  
         new GoToCommand(
             swerve,
             new Pose2d(1.9453771028, 2.743581,
                 Rotation2d.fromDegrees(0))),
-        new SetArmState(ArmStates.TRANSIT, arm),        
+             
         new GoToCommand(
             swerve,
             new Pose2d(3.279775, 2.743581,
