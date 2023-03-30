c5316ee7318764be0b736c5d4a27303ab4b2401f
SeanCh7 <62511636+SeanCh7@users.noreply.github.com>
2023-03-29 20:56:08 -0500

    more scripts

--- DSScripts/DSSetup.bat
+++ DSScripts/DSSetup.bat
@@ -1,4 +1,4 @@
-::Close th driver station
+::Close the driver station
 taskkill /IM "DriverStation.exe"
 
 ::Force close shuffleboard so there is no save dialog, which prevents it from restarting
@@ -10,8 +10,8 @@ taskkill /IM "electron.exe"
 netsh advfirewall set allprofiles state off 
 
 ::Disable Adapters (competition only)
-::netsh interface set interface "Wi-Fi" DISABLED
-::netsh interface set interface "Wi-Fi 2" DISABLED
+netsh interface set interface "Wi-Fi" DISABLED
+netsh interface set interface "Wi-Fi 2" DISABLED
 
 :: 2 seconds is the minimum, 1.9 doesn't allow time to close and reopen
 timeout /t 2
@@ -20,5 +20,10 @@ timeout /t 2
 Start /max "" "C:\Program Files (x86)\FRC Driver Station\DriverStation.exe"
 
 ::Start twilightdash
-cd "C:\Users\User\Documents\GitHub\2023-robot\TwighlightDash"
-npm run start
+::cd "C:\Users\User\Documents\GitHub\2023-robot\TwighlightDash"
+::npm run start
+
+"C:\Users\Blue Twilight\Desktop\chooseauto.txt"
+
+timeout /t 30
+taskkill "notepad.exe"
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Base code for the 2220 swerve-type robot. Any drivetrain or twilight library related code should reside here, and any other code should reside in its respective years repo.
 
 [![Format](https://github.com/Team2220/2220-swerve/actions/workflows/autoformat.yml/badge.svg)](https://github.com/Team2220/2220-swerve/actions/workflows/autoformat.yml)
+
+
+DSScript icons by Vector Market on flaticon.com
