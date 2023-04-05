a892f015d8291fcfca76fb4cf7f457e2973210df
notJithin <123132870+notJithin@users.noreply.github.com>
2023-04-04 20:47:00 -0500

    new wire bump auto

---
+++ src/main/java/frc/robot/auto/Finished/Score1WireBump.java
@@ -0,0 +1,70 @@
+// unfinished
+
+package frc.robot.auto.Finished;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Balancing;
+import frc.robot.commands.Arm.AutoShoulderState;
+import frc.robot.commands.Arm.AutoWristState;
+import frc.robot.commands.Arm.SetArmState;
+import frc.robot.commands.Intake.AutoIntake;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Arm.ArmStates;
+import frc.robot.subsystems.Intake;
+import frc.twilight.swerve.commands.GoToCommand;
+import frc.twilight.swerve.subsystems.Swerve;
+
+public class Score1WireBump extends SequentialCommandGroup {
+    public Score1WireBump(Swerve swerve, Arm arm, Intake intake) {
+        addCommands(
+                new InstantCommand(
+                        () -> swerve.setPose2d(
+                                new Pose2d(
+                                        new Translation2d(1.8453771028,
+                                                1.071499),
+                                        Rotation2d.fromDegrees(180)))), // set
+                                                                        // starting
+                                                                        // position
+                // score starting game piece
+                new JustScoreTheCubeHigh(swerve, arm, intake),
+                // score game piece 1
+                new GoToCommand(
+                        swerve,
+                        new Pose2d(1.8453771028, 0.919099,
+                                Rotation2d.fromDegrees(180)))
+                        .alongWith(new SetArmState(ArmStates.TRANSIT, arm)),
+                new GoToCommand(
+                        swerve,
+                        new Pose2d(7.070376, 0.919099,
+                                Rotation2d.fromDegrees(0)))
+                        .alongWith(new SetArmState(ArmStates.INTAKE, arm))
+                        .raceWith(new AutoIntake(-.5, intake)),
+                new SetArmState(ArmStates.TRANSIT, arm)
+                        .raceWith(new AutoIntake(-.5, intake).withTimeout(.2))
+                        .alongWith(new GoToCommand(
+                                swerve,
+                                new Pose2d(1.8453771028, 0.919099,
+                                        Rotation2d.fromDegrees(180)))),
+                new GoToCommand(
+                        swerve,
+                        new Pose2d(1.8453771028, 1.071499,
+                                Rotation2d.fromDegrees(180))),
+                new JustScoreTheCubeHigh(swerve, arm, intake));
+        // new SetArmState(ArmStates.TRANSIT, arm));
+        // balance
+        // new GoToCommand(
+        // swerve,
+        // new Pose2d(1.9453771028, 2.743581,
+        // Rotation2d.fromDegrees(0))),
+        // new GoToCommand(
+        // swerve,
+        // new Pose2d(3.279775, 2.743581,
+        // Rotation2d.fromDegrees(0)))
+        // .setTolerence(0.2, 10),
+        // new Balancing(swerve));
+    }
+}
