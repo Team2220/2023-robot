836cda5d73fca7c7b876d922105929a9e5c45f29
notJithin <123132870+notJithin@users.noreply.github.com>
2023-04-08 17:16:52 -0500

    updated  auto

--- src/main/java/frc/robot/auto/Finished/V3CenterBal.java
+++ src/main/java/frc/robot/auto/Finished/V3CenterBal.java
@@ -3,7 +3,6 @@ package frc.robot.auto.Finished;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.auto.SimpleAutos.JustScoreTheCubeHigh;
@@ -15,40 +14,45 @@ import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Arm.ArmStates;
 import frc.twilight.swerve.commands.GoToCommand;
 import frc.twilight.swerve.subsystems.Swerve;
-import frc.twilight.swerve.vectors.Position;
 
 public class V3CenterBal extends SequentialCommandGroup {
-    public V3CenterBal(Swerve swerve, Arm arm, Intake intake) {
-        addCommands(
-                new InstantCommand(
-                        () -> swerve.setPose2d(
-                                new Pose2d(
-                                        new Translation2d(1.8453771028, 2.747899),
-                                        Rotation2d.fromDegrees(180)))), // set
-                                                                        // starting
-                                                                        // position
-                // score starting game piece
-                new JustScoreTheCubeHigh(swerve, arm, intake),
-                // score game piece 1
-                new GoToCommand(
-                        swerve,
-                        new Pose2d(2.952708, 2.747899 + 0.377830 + 1.216025 + 0.379072 + 0.4138011028,
-                                Rotation2d.fromDegrees(0)))
-                        .alongWith(new SetArmState(ArmStates.TRANSIT, arm)),
-                new GoToCommand(
-                        swerve,
-                        new Pose2d(7.057840, 2.134508,
-                                Rotation2d.fromDegrees(0)))
-                        .alongWith(new SetArmState(ArmStates.INTAKE, arm))
-                        .raceWith(new AutoIntake(-.5, intake).withTimeout(2.5)),
-                new SetArmState(ArmStates.TRANSIT, arm)
-                        .raceWith(new AutoIntake(-.5, intake).withTimeout(.2))
-                        .alongWith(new GoToCommand(
-                                swerve,
-                                new Pose2d(2.952708, 2.747899 + 0.377830 + 2.747899 + 0.377830,
-                                        Rotation2d.fromDegrees(0)))),
-                // balance
-                new Balancing(swerve),
-                new JustScoreTheCubeHigh(swerve, arm, intake));
-    }
+        public V3CenterBal(Swerve swerve, Arm arm, Intake intake) {
+                addCommands(
+                                new InstantCommand(
+                                                () -> swerve.setPose2d(
+                                                                new Pose2d(
+                                                                                new Translation2d(1.8453771028,
+                                                                                                2.747899),
+                                                                                Rotation2d.fromDegrees(180)))), // set
+                                                                                                                // starting
+                                                                                                                // position
+                                // score starting game piece
+                                new JustScoreTheCubeHigh(swerve, arm, intake),
+                                // score game piece 1
+                                new GoToCommand(
+                                                swerve,
+                                                new Pose2d(2.952708,
+                                                                2.747899 + 0.377830 + 1.216025 + 0.379072
+                                                                                + 0.4138011028,
+                                                                Rotation2d.fromDegrees(0)))
+                                                .alongWith(new SetArmState(ArmStates.TRANSIT, arm)),
+                                new GoToCommand(
+                                                swerve,
+                                                new Pose2d(7.057840, 2.134508,
+                                                                Rotation2d.fromDegrees(0)))
+                                                .alongWith(new SetArmState(ArmStates.INTAKE, arm))
+                                                .raceWith(new AutoIntake(-.5, intake).withTimeout(2.5)),
+                                new SetArmState(ArmStates.TRANSIT, arm)
+                                                .raceWith(new AutoIntake(-.5, intake).withTimeout(.2))
+                                                .alongWith(new GoToCommand(
+                                                                swerve,
+                                                                new Pose2d(2.952708,
+                                                                                2.747899 + 0.377830 + 2.747899
+                                                                                                + 0.377830,
+                                                                                Rotation2d.fromDegrees(0)))),
+                                // balance
+                                new Balancing(swerve),
+                                // if wanting to shoot gamepiece at end of auto
+                                new JustScoreTheCubeHigh(swerve, arm, intake));
+        }
 }
