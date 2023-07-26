d69635ea312673b7698a92c9ba405448e1121869
ethankrech <87047924+ethankrech@users.noreply.github.com>
2023-07-25 20:43:26 -0500

    Update AngularTalonFX.java

--- src/main/java/frc/robot/subsystems/AngularTalonFX.java
+++ src/main/java/frc/robot/subsystems/AngularTalonFX.java
@@ -1,15 +1,11 @@
 package frc.robot.subsystems;
 
-import java.util.ArrayList;
-import java.util.Map;
-
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.RobotController;
@@ -23,45 +19,63 @@ import frc.robot.Constants;
 import frc.robot.Constants.ArmConfig;
 import frc.robot.commands.Arm.SetArmState;
 import frc.twilight.tunables.TunableDouble;
+import java.util.ArrayList;
+import java.util.Map;
 
 class AngularTalonFX {
-    private DutyCycleEncoder dutyCycleEncoder;
-    private TalonFX talonFX;
-    
-    private final TunableDouble talonP;
-    private final TunableDouble talonI;
-    private final TunableDouble talonD;
-    private double oldTalonP;
-    private double oldTalonI;
-    private double oldTalonD;
 
-    private TalonFXConfiguration talonConfig = new TalonFXConfiguration();
+  private DutyCycleEncoder dutyCycleEncoder;
+  private TalonFX talonFX;
 
-    public AngularTalonFX(
-        int dutyEncoder, int talonId, String name, boolean tunableDoubleEnabled, double gearRatio,
-        boolean inverted
-        ) {
-        dutyCycleEncoder = new DutyCycleEncoder(dutyEncoder);
-        talonFX = new TalonFX(talonId);
+  private final TunableDouble talonP;
+  private final TunableDouble talonI;
+  private final TunableDouble talonD;
+  private double oldTalonP;
+  private double oldTalonI;
+  private double oldTalonD;
 
-        talonP = new TunableDouble(name + "P", 0.1, tunableDoubleEnabled);
-        talonI = new TunableDouble(name + "I", 0, tunableDoubleEnabled);
-        talonD = new TunableDouble(name + "D", 0.2, tunableDoubleEnabled);
-        oldTalonP = talonP.getValue();
-        oldTalonI = talonI.getValue();
-        oldTalonD = talonD.getValue();
+  private TalonFXConfiguration talonConfig = new TalonFXConfiguration();
 
-        talonConfig.motionAcceleration = degreesPerSecondToEncoderTicks(200, gearRatio);
-        talonConfig.motionCruiseVelocity = degreesPerSecondToEncoderTicks(200, gearRatio);
+  public AngularTalonFX(
+    int dutyEncoder,
+    int talonId,
+    String name,
+    boolean tunableDoubleEnabled,
+    double gearRatio,
+    boolean inverted
+  ) {
+    dutyCycleEncoder = new DutyCycleEncoder(dutyEncoder);
+    talonFX = new TalonFX(talonId);
 
-        talonFX.configAllSettings(talonConfig);
-        talonFX.configVoltageCompSaturation(10);
-        talonFX.setInverted(inverted);
-  
-    }
+    talonP = new TunableDouble(name + "P", 0.1, tunableDoubleEnabled);
+    talonI = new TunableDouble(name + "I", 0, tunableDoubleEnabled);
+    talonD = new TunableDouble(name + "D", 0.2, tunableDoubleEnabled);
+    oldTalonP = talonP.getValue();
+    oldTalonI = talonI.getValue();
+    oldTalonD = talonD.getValue();
 
-    public void updatePID() {
-         if (talonP.getValue() != oldTalonP) {
+    talonConfig.motionAcceleration =
+      degreesPerSecondToEncoderTicks(200, gearRatio);
+    talonConfig.motionCruiseVelocity =
+      degreesPerSecondToEncoderTicks(200, gearRatio);
+
+    talonFX.configAllSettings(talonConfig);
+    talonFX.configVoltageCompSaturation(10);
+    talonFX.setInverted(inverted);
+
+    SupplyCurrentLimitConfiguration supplyConfig = new SupplyCurrentLimitConfiguration();
+    supplyConfig.currentLimit = 20;
+    supplyConfig.enable = true;
+    talonFX.configSupplyCurrentLimit(supplyConfig);
+
+    StatorCurrentLimitConfiguration statorConfig = new StatorCurrentLimitConfiguration();
+    statorConfig.currentLimit = 33;
+    statorConfig.enable = true;
+    talonFX.configStatorCurrentLimit(statorConfig);
+  }
+
+  public void updatePID() {
+    if (talonP.getValue() != oldTalonP) {
       talonFX.config_kP(0, talonP.getValue());
       oldTalonP = talonP.getValue();
     }
@@ -75,10 +89,17 @@ class AngularTalonFX {
       talonFX.config_kD(0, talonD.getValue());
       oldTalonD = talonD.getValue();
     }
-    }
+  }
 
-    private double degreesPerSecondToEncoderTicks(double angle, double gearRatio) {
-      double gfx = ((angle / 360.0) * gearRatio) * ArmConfig.TALONFX_ENCODER_TICKS * 1.0 / 10.0;
-      return gfx;
+  private double degreesPerSecondToEncoderTicks(
+    double angle,
+    double gearRatio
+  ) {
+    double gfx =
+      ((angle / 360.0) * gearRatio) *
+      ArmConfig.TALONFX_ENCODER_TICKS *
+      1.0 /
+      10.0;
+    return gfx;
   }
 }
