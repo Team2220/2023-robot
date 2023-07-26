53e7d24975f40dbb175fd763f8e5c3fd68559128
ethankrech <87047924+ethankrech@users.noreply.github.com>
2023-07-25 20:12:18 -0500

    Create AngularTalonFX.java

---
+++ src/main/java/frc/robot/subsystems/AngularTalonFX.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.ArmConfig;
+import frc.robot.commands.Arm.SetArmState;
+import frc.twilight.tunables.TunableDouble;
+
+class AngularTalonFX {
+    private DutyCycleEncoder dutyCycleEncoder;
+
+    public AngularTalonFX(
+        int dutyEncoder
+        ) {
+        dutyCycleEncoder = new DutyCycleEncoder(dutyEncoder);
+    }
+}
